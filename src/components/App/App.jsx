b0b74ba6b8e2742320774e984a6b87bc59bee746
Igor <105238566+IgorZhyvotkov@users.noreply.github.com>
2023-06-03 11:32:26 +0300

    localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,10 +23,12 @@ class App extends Component {
     const contacts = localStorage.getItem('contacts')
     const parsedContacts = JSON.parse(contacts)
 
+  if(parsedContacts){
     this.setState({contacts: parsedContacts})
   }
+  }
 
-  componentDidUpdate( prevProps, prevState){
+  componentDidUpdate( _, prevState){
     if(this.state.contacts !== prevState.contacts){
       localStorage.setItem('contacts' , JSON.stringify(this.state.contacts))
     }
