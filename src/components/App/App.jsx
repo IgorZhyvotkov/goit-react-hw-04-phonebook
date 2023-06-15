1530a7f8cad95038a0629794dbf974eb6f4139a4
Igor <105238566+IgorZhyvotkov@users.noreply.github.com>
2023-06-15 12:47:46 +0300

    asd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,15 +6,17 @@ import ContactForm from 'components/ContactForm';
 import Filter from 'components/Filter';
 import ContactList from 'components/ContactList';
 
+const INITUAL_STATE = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 function App() {
   const [contacts, setContacts] = useState(
     () =>
-      JSON.parse(window.localStorage.getItem('contacts')) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
+      JSON.parse(window.localStorage.getItem('contacts')) ?? INITUAL_STATE
   );
   const [filter, setFilter] = useState('');
 
@@ -25,12 +27,12 @@ function App() {
   const onAddContact = inputVelues => {
     const id = nanoid();
     const newContact = { id, ...inputVelues };
-    for (let contact of contacts) {
-      if (inputVelues.name.toLowerCase() === contact.name.toLowerCase()) {
+
+    if(contacts.find(contact => contact.name === inputVelues)) {      
         alert(`${inputVelues.name} is already in contacts`);
-        return;
-      }
+        return;      
     }
+
     setContacts(prevState => [newContact, ...prevState]);
   };
 
